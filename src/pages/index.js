8440e23ef61c5b578a7b1b1e161dda6c22621137
geerma <geerm1997@gmail.com>
2023-02-19 10:09:48 -0700

    Welcome page final done

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Welcome.module.css";
 import Navbar from "../components/navbar";
 
-import Title from "../assets/title.png"
+import Title from "../assets/title.png";
 import Star from "../assets/star.png";
 import StarAnimation from "../assets/star-animation.gif";
 import BurgerImage from "../assets/logo.png";
@@ -14,12 +14,20 @@ import StudyIcon from "../assets/icon-study.png";
 import Login from "@/components/login";
 import Register from "@/components/register";
 
+import { auth } from "../firebase/firebase";
+
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   const [login, setLogin] = useState(false);
   const [register, setRegister] = useState(false);
+
+  const [signedIn, setSignedIn] = useState(false);
+
+  const router = useRouter();
 
   const toggleLogin = () => {
     setLogin(!login);
@@ -29,6 +37,35 @@ export default function Home() {
     setRegister(!register);
   };
 
+  const handleLogout = () => {
+    console.log("Logging out");
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out Sucessful");
+        location.reload();
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  const handleStudy = () => {
+    console.log('study');
+    router.push('/studymap');
+  }
+
+  const handleBite = () => {
+    router.push('/foodmap');
+  }
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("You are signed in as", user);
+        setSignedIn(true);
+      }
+    });
+  }, []);
 
   return (
     <>
@@ -43,24 +80,32 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.titlelogo}>
-        <Image src={Title} width={300} alt="titlelogo" />
+          <Image src={Title} width={300} alt="titlelogo" />
         </div>
         <Image src={BurgerImage} width={400} height={400} alt="burgerimage" />
         <div className={styles.staricon}>
-          <Image src={StarAnimation} width={150} height={150} alt="staranimation" />
+          <Image
+            src={StarAnimation}
+            width={150}
+            height={150}
+            alt="staranimation"
+          />
         </div>
         <div className={styles.welcomeicons}>
-          <Image src={StudyIcon} width={150} height={150} alt="studyicon" />
-          <Image src={BurgerIcon} width={150} height={150} alt="burgericon" />
+          <Image src={StudyIcon} width={150} height={150} alt="studyicon" onClick={() => handleStudy()}/>
+          <Image src={BurgerIcon} width={150} height={150} alt="burgericon" onClick={() => handleBite()}/>
         </div>
         <div className={styles.welcomeicons}>
           <h2>Study</h2>
           <h2>Bite</h2>
         </div>
-        <div className={styles.welcomebuttons}>
-          <button onClick={toggleLogin}>Login</button>
-          <button onClick={toggleRegister}>Register</button>
-        </div>
+        {signedIn == false ? (
+          <div className={styles.welcomebuttons}>
+            <button onClick={toggleLogin}>Login</button>
+            <button onClick={toggleRegister}>Register</button>
+          </div>
+        ) : <button onClick={handleLogout}>Logout</button>}
+
         {login && <Login handleClose={toggleLogin} />}
         {register && <Register handleClose={toggleRegister} />}
       </main>
