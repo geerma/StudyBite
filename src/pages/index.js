d1bcab94fea32bde6564ab1e5d13374f919d2e6f
unknown <geerm1997@gmail.com>
2023-02-18 20:36:55 -0700

    Fix axios package

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
     <>
       <Head>
         <title>Restudy</title>
-        <meta name="description" content="Rate restaurnay and study place on campus" /
+        <meta name="description" content="Rate restaurnay and study place on campus" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
