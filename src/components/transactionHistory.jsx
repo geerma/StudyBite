eea4c73d9fd3a3e2d1f11b2b2501015d6e0adf27
Joshua Lee <josh@Joshuas-MacBook-Air.local>
2023-02-19 02:04:53 -0700

    playing with dropdown

--- src/components/transactionHistory.jsx
+++ src/components/transactionHistory.jsx
@@ -1,18 +1,33 @@
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/Dropdown.module.css";
+
 const TransactionHistory = ({ userData }) => {
     const transactions = userData.transactionHistory;
+    const [display, setDisplay] = useState('none');
+    
+    const handleClick = () => {
+        console.log("clicked");
+        console.log(display);
+        if(display == 'none'){
+            setDisplay('block');
+        } else {
+            setDisplay('none');
+        }
+    }
 
   return (
-    <div>
-        {
-            transactions && transactions.map((item, id) => (
-                <div key={id}>
-                    <p>Location: { item.Location}</p>
-                    <p>Total: ${ item.finalCostCents / 100}</p>
-                    <p>Time: { item.time}</p>
-                </div>
-            ))
-        }
-    </div>
+        <div>
+            <div className={styles.dropdown} onClick={handleClick}>Transaction History</div>
+            <div style={{display:display}}>
+                {transactions && transactions.map((item, id) => (
+                    <div key={id}>
+                        <p>Location: { item.Location}</p>
+                        <p>Total: ${ item.finalCostCents / 100}</p>
+                        <p>Time: { item.time}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
   );
 };
 
--- src/pages/user-profile.js
+++ src/pages/user-profile.js
@@ -2,35 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '@/firebase/firebase';
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import TransactionHistory from '@/components/transactionHistory';
 
 const inter = Inter({ subsets: ['latin'] })
-// onAuthStateChanged(auth, (user) => {
-    //     if (user) {    
-        //         uid = user.uid;
-        //     }
-        //     else {
-            //         uid = null;
-            //         //need to get them to sign in
-            //     }
-            // });
-            
             
 export default function userProfile() {
     
-    //hacky fix to prevent useEffect infinite loop :(
-    const [user, setUser] = useState({});
     const [userData, setUserData] = useState({});
+    // const [dropdown, setDropdown] = useState(false);
 
-    let listTransactions = [];
-    
     useEffect(() => {
         getUserData();
-    }, [user]);
+    }, []);
     
     const uid = '8PBspZtOC8jdQtaLw9P9';
     
@@ -77,75 +63,15 @@ export default function userProfile() {
             </a>
           </div>
         </div>
-
         <div className={styles.center}>
             <h2 className={inter.className}>
                Hello, {userData.firstName} {userData.lastName}
             </h2>
         </div>
-
-        <div className={styles.center}>
+        <div>
             <TransactionHistory userData={userData}/>
         </div>
 
-        {/* <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
     </>
   )
