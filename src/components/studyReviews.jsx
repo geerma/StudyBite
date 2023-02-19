514d853b3e99e82df5e25c17f4b3a5af011698ab
unknown <geerm1997@gmail.com>
2023-02-18 19:45:59 -0700

    Update study reviews

--- src/components/studyReviews.jsx
+++ src/components/studyReviews.jsx
@@ -1,12 +1,17 @@
 import styles from "@/styles/Study.module.css";
 
-const StudyReviews = ({review}) => {
+const StudyReviews = ({ review }) => {
   return (
     <div className={styles.studyreview_box}>
-      <p>Overall: {review.overallRating}</p>
-      <p>Quietness: {review.quietness}</p>
-      <p>Outlets: {review.outlets}</p>
-      <p>Seating {review.seating}</p>
+      <div className={styles.studyratings}>
+        <p>Overall: {review.overallRating}</p>
+        <p>Outlets: {review.outlets}</p>
+        <p>Seating: {review.seating}</p>
+        <p>Quietness: {review.quietness}</p>
+      </div>
+      <div className={styles.studycomments}>
+        <p>Comments: {review.comments}</p>
+      </div>
     </div>
   );
 };
